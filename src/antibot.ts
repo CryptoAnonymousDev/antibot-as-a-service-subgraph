cf39b4fcfdc4ed7293342170c8c04c4ad8444047
Sebastian Bauer <sebastian.bauer9393@gmail.com>
2022-09-14 22:01:49 +0200

    MAPPINGS : Implemented mapping for 'MarkedBlacklisted' event

--- src/antibot.ts
+++ src/antibot.ts
@@ -15,7 +15,7 @@ import {
   TradingStartChanged,
   Upgraded
 } from "../generated/Antibot/Antibot"
-import { Owner, Token, User } from "../generated/schema"
+import { BlacklistedAccount, Owner, Token, User } from "../generated/schema"
 
 export function handleAdminChanged(event: AdminChanged): void {}
 
@@ -69,7 +69,43 @@ export function handleBeaconUpgraded(event: BeaconUpgraded): void {}
 
 export function handleInitialized(event: Initialized): void {}
 
-export function handleMarkedBlacklisted(event: MarkedBlacklisted): void {}
+export function handleMarkedBlacklisted(event: MarkedBlacklisted): void {
+  let token = Token.load(event.params.token);
+
+  if (!token) {
+    token = new Token(event.params.token);
+
+    token.antibotActive = false;
+    token.tradingStart = BigInt.fromI32(0);
+    token.maxTransferAmount = BigInt.fromI32(0);
+
+    token.save();
+  }
+
+  let user = User.load(event.params.account);
+
+  if (!user) {
+    user = new User(event.params.account);
+
+    user.save();
+  }
+
+  let blacklistedAccount = BlacklistedAccount.load(event.params.token.concat(event.params.account));
+
+  if (event.params.isBlacklisted) {
+    if (!blacklistedAccount) {
+      blacklistedAccount = new BlacklistedAccount(event.params.token.concat(event.params.account));
+
+      blacklistedAccount.token = token.id;
+      blacklistedAccount.user = user.id;
+
+      blacklistedAccount.save();
+    }
+  } else {
+    if (blacklistedAccount)
+      store.remove('BlacklistedAccount', event.params.token.concat(event.params.account).toHexString());
+  }
+}
 
 export function handleMarkedProtected(event: MarkedProtected): void {}
 

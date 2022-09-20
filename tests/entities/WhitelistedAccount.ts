8ad17a38f05e94df06f3bd92aa277a2cf78c00a2
Sebastian Bauer <sebastian.bauer9393@gmail.com>
2022-09-20 22:26:39 +0200

    UNIT TESTS : Created 'WhitelistedAccount' entity

---
+++ tests/entities/WhitelistedAccount.ts
@@ -0,0 +1,7 @@
+export class WhitelistedAccount {
+    id: string = 'id';
+    token: string = 'token';
+    user: string = 'user';
+}
+
+export const WHITELISTED_ACCOUNT = new WhitelistedAccount();

9c038f5e41a68e78f10e0aa4d7bf2dbc1ae6700d
Sebastian Bauer <sebastian.bauer9393@gmail.com>
2022-09-20 22:25:09 +0200

    UNIT TESTS : Created constants used in unit tests

---
+++ tests/constants/constants.ts
@@ -0,0 +1,7 @@
+export const OWNER_ENTITY_TYPE: string = 'Owner';
+export const TOKEN_ENTITY_TYPE: string = 'Token';
+export const USER_ENTITY_TYPE: string = 'User';
+export const BLACKLISTED_ACCOUNT_ENTITY_TYPE: string = 'BlacklistedAccount';
+export const WHITELISTED_ACCOUNT_ENTITY_TYPE: string = 'WhitelistedAccount';
+export const PROTECTED_ACCOUNT_ENTITY_TYPE: string = 'ProtectedAccount';
+export const UNTHROTTLED_ACCOUNT_ENTITY_TYPE: string = 'UnthrottledAccount';

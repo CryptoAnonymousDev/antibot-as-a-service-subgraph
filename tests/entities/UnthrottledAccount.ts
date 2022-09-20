a087a3a710cb91381c17ccdb95be9d8a4797d621
Sebastian Bauer <sebastian.bauer9393@gmail.com>
2022-09-20 22:26:26 +0200

    UNIT TESTS : Created 'UnthrottledAccount' entity

---
+++ tests/entities/UnthrottledAccount.ts
@@ -0,0 +1,7 @@
+export class UnthrottledAccount {
+    id: string = 'id';
+    token: string = 'token';
+    user: string = 'user';
+}
+
+export const UNTHROTTLED_ACCOUNT = new UnthrottledAccount();
